17c44ebc8fd40bfc81e62c4fbb044dc0607b63a1
lgfree2010 <lgfree2010@163.com>
2026-04-07 15:04:00 +0800

    增加几个边界函数

--- week1_string_utils/test_1.py
+++ week1_string_utils/test_1.py
@@ -12,3 +12,15 @@ def test_sum_equals_0():
 
 def test_sum_negative():
     assert my_sum([-1, -2, -3]) == -6
+
+def test_sum_float():
+    assert my_sum([1.6,2.4,3.0]) == 7.0
+
+def test_sum_mixed():
+    assert my_sum([1, -2, 3]) == 2
+
+def test_sum_large():
+    assert my_sum([1000000, 2000000, 3000000]) == 6000000
+
+def test_sum_single_element():
+    assert my_sum([5]) == 5
